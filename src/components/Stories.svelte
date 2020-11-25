fdccb5fb9b10966a077d13e4a23bdd5dcf064bdb
Hallef Santos <hallefsantos@gmail.com>
2020-11-25 20:16:19 -0300

    Add Stories component

---
+++ src/components/Stories.svelte
@@ -0,0 +1,10 @@
+<script>
+   import Story from './Story.svelte'
+   export let stories = []
+</script>
+
+<ul class="px-4 py-3 flex space-x-4 overflow-x-auto">
+   {#each stories as story, i}
+      <Story {story} index={i} />
+   {/each}
+</ul>
