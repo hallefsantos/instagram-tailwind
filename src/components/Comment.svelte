977add6c745ab35f0bee89f73b0cae45684ca453
Hallef Santos <hallefsantos@gmail.com>
2020-11-25 20:15:57 -0300

    Add comment component

---
+++ src/components/Comment.svelte
@@ -0,0 +1,14 @@
+<script>
+   import Icon from './Icon.svelte'
+   export let username
+</script>
+
+<div class="flex items-start">
+   <p class="flex-auto">
+      <a class="font-bold" href="#">{username}</a>
+      <slot />
+   </p>
+   <button class="ml-2">
+      <Icon class="h-4 w-4" name="heart" />
+   </button>
+</div>
