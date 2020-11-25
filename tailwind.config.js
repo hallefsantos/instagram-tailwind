5e004a1a7ca60f774e4c724909bb18e4f62c69c8
Hallef Santos <hallefsantos@gmail.com>
2020-11-25 20:14:41 -0300

    Genesis

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      red: "#ed4956",
+      white: "#FFF",
+      black: "#000",
+      blue: "#0095F6",
+      pink: "#DD2A7B",
+      purple: "#8134AF",
+      orange: "#F58529",
+      gray: {
+        'light': '#F3F4F6',
+        'dark': '#8e8e8e'
+      },
+    },
+    extend: {},
+    fontFamily: {
+      // 'sans': ['Roboto Condensed', 'sans-serif']
+    },
+  },
+  variants: {},
+  plugins: []
+}
