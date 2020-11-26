5e701635ae6bb6d0040aefd8390fed3fe933d55f
Hallef Santos <hallefsantos@gmail.com>
2020-11-25 23:12:07 -0300

    Add Buttons component

---
+++ src/components/ButtonDirect.svelte
@@ -0,0 +1,19 @@
+<script>
+   import Icon from './Icon.svelte'
+
+   export let direct = 0
+</script>
+
+<button class="relative">
+   <Icon 
+      class="h-6 w-6 transform rotate-45" 
+      name="direct" 
+   />
+   {#if direct > 0}
+      <span 
+         class="absolute right-0 top-0 h-4 w-4 flex items-center justify-center bg-red text-xs text-white rounded-full"
+      >
+         {direct}
+      </span>
+   {/if}
+</button>
---
+++ src/components/ButtonProfile.svelte
@@ -0,0 +1,7 @@
+<button>
+   <img 
+      class="w-7 h-7 rounded-full object-cover object-center" 
+      src="https://placekitten.com/30/30" 
+      alt="@haalefs"
+   >
+</button>
