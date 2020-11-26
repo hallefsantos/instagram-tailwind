b528be1a77f94477f966b348373c2769df481650
Hallef Santos <hallefsantos@gmail.com>
2020-11-25 23:11:49 -0300

    Add Search component

---
+++ src/components/Search.svelte
@@ -0,0 +1,44 @@
+<script>
+   import Icon from './Icon.svelte'
+
+   let isFocused = false
+   let searchInput
+
+   function focus () {
+      if(!searchInput.value > 0) {
+         isFocused = !isFocused
+      }
+   }
+
+   function clearInput () {
+      searchInput.value = ''
+      isFocused = false
+   }
+
+   $: console.log(searchInput)
+</script>
+
+<div class="relative">
+   <div 
+      class="{ isFocused ? 'ml-2' : 'ml-16' } absolute inset-y-0 flex items-center opacity-40"
+   >
+      <Icon class="h-3 w-3" name="search"/>
+   </div>
+   <input 
+      bind:this={searchInput}
+      on:focus={focus} 
+      on:focusout={focus} 
+      class="{ isFocused ? 'text-left' : 'text-center' } py-1 px-6 w-52 bg-gray-light focus:outline-none border border-gray-dark text-sm rounded" 
+      type="text" 
+      placeholder="Search"
+   >
+   {#if isFocused}
+      <div class="absolute right-1 inset-y-0 flex items-center">
+         <button 
+            on:click={clearInput}
+            class="opacity-50 hover:opacity-90 transition duration-200">
+            <Icon class="h-5 w-5" name="close"/>
+         </button>
+      </div>
+   {/if}
+</div>
